2dae785b1563fe94177989899e35827981cf5649
Erick <ericknevarez97@gmail.com>
2020-04-24 15:59:24 -0600

    Buttons are now clickable in sidebar

--- lib/commons/CollapsingListTile.dart
+++ lib/commons/CollapsingListTile.dart
@@ -6,8 +6,15 @@ class CollapsingListTile extends StatefulWidget {
   final String title;
   final IconData icon;
   final AnimationController animationController;
+  final bool isSelected;
+  final Function onTap;
 
-  CollapsingListTile({@required this.title, @required this.icon, @required this.animationController});
+  CollapsingListTile({
+    @required this.title, 
+    @required this.icon, 
+    @required this.animationController, 
+    this.isSelected = false, 
+    this.onTap});
   @override
   _CollapsingListTileState createState() => _CollapsingListTileState();
 }
@@ -25,18 +32,28 @@ class _CollapsingListTileState extends State<CollapsingListTile> {
   
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: widthAnimation.value,
-      margin: EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
-      child: Row(
-        children: <Widget>[
-          Icon(widget.icon, color:  Colors.white30, size: 38.0,),
-          SizedBox(width: sizedAnimation.value),
-          (widthAnimation.value >= 220) ? 
-            Text(widget.title, style: listTitleDefaultTextStyle,) : 
-            Container()
-
-        ],
+    return InkWell(
+      onTap: widget.onTap,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(16.0)),
+          color: widget.isSelected ? 
+            Colors.transparent.withOpacity(0.3) :
+            Colors.transparent,
+        ),
+        width: widthAnimation.value,
+        margin: EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),          child: Row(
+          children: <Widget>[
+            Icon(widget.icon, color:  Colors.white30, size: 38.0,),
+            SizedBox(width: sizedAnimation.value),
+            (widthAnimation.value >= 220) ? 
+              Text(widget.title, 
+                style: widget.isSelected ? 
+                  listTitleSelectedTextStyle : 
+                  listTitleDefaultTextStyle,) : 
+              Container()
+          ],
+        ),
       ),
     );
   }
--- lib/commons/CollapsingNavigationDrawer.dart
+++ lib/commons/CollapsingNavigationDrawer.dart
@@ -22,6 +22,7 @@ class CollapsingNavigationDrawerState extends State<CollapsingNavigationDrawer> 
   bool isCollapsed = false;
   AnimationController _animationController;
   Animation<double> widthAnimation;
+  int currentSelectedIndex = 0;
 
   @override
   void initState() { 
@@ -36,44 +37,59 @@ class CollapsingNavigationDrawerState extends State<CollapsingNavigationDrawer> 
     }
     
   Widget getWidget(BuildContext context, Widget widget) {
-    return Container(
-      width: widthAnimation.value,
-      color: drawerBackgroundColor,
-      child: Column(
-        children: <Widget>[
-          SizedBox(height: 50,),
-          CollapsingListTile(
-            //TODO Grab user login info and put it here.
-            title: 'User Name',
-            icon: Icons.person,
-            animationController: _animationController,
-          ),
-          Expanded(
-              child: ListView.builder(itemBuilder: (context, counter) {
-                return CollapsingListTile(
-                  title: navigationItems[counter].title,
-                  icon: navigationItems[counter].icon,
-                  animationController: _animationController);
+    return Material(
+      elevation: 8.0,
+      child: Container(
+        width: widthAnimation.value,
+        color: drawerBackgroundColor,
+        child: Column(
+          children: <Widget>[
+            CollapsingListTile(
+              //TODO Grab user login info and put it here.
+              title: 'User Name',
+              icon: Icons.person,
+              animationController: _animationController,
+            ),
+            Divider(color: Colors.grey),
+            Expanded(
+                child: ListView.separated(
+                  
+                  separatorBuilder: (context, counter) {
+                    return Divider(color: Colors.grey, height:12.0);
+                  },
+                  
+                  itemBuilder: (context, counter) {
+                  return CollapsingListTile(
+                    onTap: () {
+                      setState(() {
+                        currentSelectedIndex = counter;
+                      });
+                    },
+                    isSelected: currentSelectedIndex == counter,
+                    title: navigationItems[counter].title,
+                    icon: navigationItems[counter].icon,
+                    animationController: _animationController);
+                },
+                itemCount: navigationItems.length,
+              ),
+            ),
+            InkWell(
+              onTap: () {
+                setState(() {
+                  isCollapsed = !isCollapsed;
+                  isCollapsed ? _animationController.forward() : _animationController.reverse();
+                });
               },
-              itemCount: navigationItems.length,
+              child: AnimatedIcon(
+                icon: AnimatedIcons.close_menu, 
+                progress: _animationController,
+                color: Colors.white, 
+                size: 50.0,
+              ),
             ),
-          ),
-          InkWell(
-            onTap: () {
-              setState(() {
-                isCollapsed = !isCollapsed;
-                isCollapsed ? _animationController.forward() : _animationController.reverse();
-              });
-            },
-            child: AnimatedIcon(
-              icon: AnimatedIcons.close_menu, 
-              progress: _animationController,
-              color: Colors.white, 
-              size: 50.0,
-            ),
-          ),
-          SizedBox(height: 50.0),
-        ]
+            SizedBox(height: 50.0),
+          ]
+        ),
       ),
     );
   }
